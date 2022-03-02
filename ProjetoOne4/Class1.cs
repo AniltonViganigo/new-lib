c5a2920671bdecd3488c3c794ce43305824c1cc3
Anilton Vigânigo Jr <viganigojr@gmail.com>
2022-03-02 15:30:20 -0300

    New Lib C#

---
+++ ProjetoOne4/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace ProjetoOne4
+{
+    public class Class1 : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("Insira o Primeiro Número")]
+        public InArgument<Double> PrimeiroNumero { get; set; }
+
+        [Category("Input")]
+        [RequiredArgument]
+        [Description("Insira o Segundo Número")]
+        public InArgument<Double> SegundoNumero { get; set; }
+
+        [Category("Output")]
+        public OutArgument<Double> Resultado { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var primeiroNumero = PrimeiroNumero.Get(context);
+            var segundoNumero = SegundoNumero.Get(context);
+            var resultado = primeiroNumero + segundoNumero;
+
+            Resultado.Set(context, resultado);
+        }
+    }
+}
